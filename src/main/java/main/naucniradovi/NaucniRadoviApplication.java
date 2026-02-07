2aed608913003cf2a6057cbf2985e41416d82068
arnelahh <arnela.hodzic.23@size.ba>
2026-02-07 23:29:51 +0100

    initial commit

---
+++ src/main/java/main/naucniradovi/NaucniRadoviApplication.java
@@ -0,0 +1,13 @@
+package main.naucniradovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaucniRadoviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaucniRadoviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/naucniradovi/NaucniRadoviApplicationTests.java
@@ -0,0 +1,13 @@
+package main.naucniradovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaucniRadoviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
